9247f255ec44abaad21408ad120305277b59473f
eeoott <luotao323.ent.jp@gmail.com>
2021-09-23 22:54:49 +0900

    19

---
+++ Framework/Common/MemoryManager.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include "IRuntimeModule.hpp"
+#include "Allocator.hpp"
+#include <new>
+
+namespace My {
+    class MemoryManager : implements IRuntimeModule {
+        public:
+            template<typename T, typename... Arguments>
+            T* New(Arguments... parameters)
+            {
+                return new (Allocator(sizeof(T))) T(paramenters...);
+            }
+            template<typename T>
+            void Delete(T* p)
+            {
+                reinterpret_cast<T*>(p)->~T();
+                Free(p, sizeof(T));
+            }
+
+        public:
+            virtual ~MemoryManager() {}
+            virtual int Initialize();
+            virtual void Finalize();
+            virtual void Tick();
+
+            void* Allocate(size_t size);
+            void Free(void* p, size_t size);
+        private :
+            static size_t* m_pBlockSizeLookup;
+            static Allocator* m_pAllocators;
+        private:
+            static Allocator* LookupAllocator(size_t size);
+    };
+}
